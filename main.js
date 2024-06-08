db3aab0db6888463d437ef7ec29703515d911f56
sadjoaldi <alsannealdi@hotmail.com>
2024-06-08 16:44:51 +0200

    little project to calculate tip, creation of all files,html,css and js, adding fonctionnalities with js and styling with css

---
+++ main.js
@@ -0,0 +1,14 @@
+const billAmount = document.querySelector("#billAmount");
+const tipPercentage = document.querySelector("#tipPercentage");
+const btnCalculate = document.querySelector("#btnCalc");
+const totalTip = document.querySelector("#total");
+
+function calculateTip(e) {}
+
+btnCalculate.addEventListener("click", (e) => {
+  e.preventDefault();
+  const bill = billAmount.value;
+  const tip = tipPercentage.value;
+  const tipValue = bill * (1 + tip / 100);
+  totalTip.innerHTML = tipValue;
+});
