e9c06d966217addaaf07eee5d16631a1c752855e
sadjoaldi <alsannealdi@hotmail.com>
2024-06-08 17:36:59 +0200

    adding some styles

--- main.js
+++ main.js
@@ -3,12 +3,10 @@ const tipPercentage = document.querySelector("#tipPercentage");
 const btnCalculate = document.querySelector("#btnCalc");
 const totalTip = document.querySelector("#total");
 
-function calculateTip(e) {}
-
 btnCalculate.addEventListener("click", (e) => {
   e.preventDefault();
   const bill = billAmount.value;
   const tip = tipPercentage.value;
   const tipValue = bill * (1 + tip / 100);
-  totalTip.innerHTML = tipValue;
+  totalTip.innerHTML = `${tipValue} €`;
 });
